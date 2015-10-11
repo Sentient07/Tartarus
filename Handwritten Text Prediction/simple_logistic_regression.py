878298c2ac946cecbc97bc2db1427f2e375f7a25
Ramana.S <vxrram95@gmail.com>
2015-10-11 23:51:13 +0530

    Added hidden layer and regularized cost function

--- Handwritten Text Prediction/simple_logistic_regression.py
+++ Handwritten Text Prediction/simple_logistic_regression.py
@@ -8,20 +8,6 @@ import theano.tensor as T
 import cPickle, gzip
 
 
-
-def encode_labels(labels, max_index):
-    """Encode the labels into binary vectors."""
-    # Allocate the output labels, all zeros.
-    encoded = np.zeros((labels.shape[0], max_index + 1))
-    
-    # Fill in the ones at the right indices.
-    for i in xrange(labels.shape[0]):
-        encoded[i, labels[i]] = 1
-    return encoded
-
-
-
-
 #Storing the dataset in shared theano variable
 def shared_dataset(data_xy, borrow=True):
 	data_x, data_y = data_xy
@@ -32,6 +18,16 @@ def shared_dataset(data_xy, borrow=True):
 				dtype=theano.config.floatX),
 				borrow=borrow)
 	return shared_x, T.cast(shared_y, 'int32')
+
+def encode_labels(labels, max_index):
+    """Encode the labels into binary vectors."""
+    # Allocate the output labels, all zeros.
+    encoded = np.zeros((labels.shape[0], max_index + 1))
+    
+    # Fill in the ones at the right indices.
+    for i in xrange(labels.shape[0]):
+        encoded[i, labels[i]] = 1
+    return encoded
 
 
 def errors(self, y):
@@ -50,6 +46,7 @@ def accuracy(predicted, actual):
     return correct / total
 
 
+
 if __name__ == '__main__':
 
 	#loading dataset
@@ -66,29 +63,56 @@ if __name__ == '__main__':
 	n_valid_batches = inp_test_set.get_value(borrow=True).shape[0] / 500
 	n_test_batches = inp_cv_set.get_value(borrow=True).shape[0] / 500
 
-	weights = (10, 784) #784 pixels, 10 possibility
+	weights = (10, 50)
 	bias = 10
 
-	#using shared function because it's a convention
-	w_shared = shared(np.random.random(weights)-0.5, name="w_shared")
-	b_shared = shared(np.random.random(bias)-0.5, name="b_shared")
+	hidden_weights = (50, 784)
+	hidden_bias = 50
+
+
+	#using shared function because it's a convenient
+	w_shared = shared(np.random.random(weights), name="w_shared")
+	b_shared = shared(np.random.random(bias), name="b_shared")
+	hidden_w_shared = shared(np.random.random(hidden_weights), name="hidden_w_shared")
+	hidden_b_shared = shared(np.random.random(hidden_bias), name="hidden_b_shared")
+
+
 	#little confusion between usage of dmatrix and dvector
 	inp = T.dmatrix("inp")
 	labels = T.dmatrix("labels")
 	#Encode labels
 
+	#The hidden layer computation
+	hidden = T.nnet.sigmoid(inp.dot(hidden_w_shared.transpose()) + hidden_b_shared)
+
 	#The softmax computation
-	output = T.nnet.softmax(inp.dot(w_shared.transpose()) + b_shared)
+	output = T.nnet.softmax(hidden.dot(w_shared.transpose()) + b_shared)
 	predict_y = T.argmax(output, axis=1)
-	predict_func = function([inp], predict_y)
+	
 	cost = T.nnet.binary_crossentropy(output, labels).mean()
 	cost_compute = function([inp, labels], cost)
+
+	#Regularization
+	cost = cost + 0.0001 * ((w_shared * w_shared).sum() + (hidden_w_shared * hidden_w_shared).sum()
+			+ (b_shared * b_shared).sum() + (hidden_b_shared * hidden_b_shared).sum())
+
+	predict_func = function([inp], predict_y)
+
+	#output 
 	weight_grad = grad(cost, w_shared)
 	bias_grad = grad(cost, b_shared)
 
+	#hidden layer
+	hidden_weight_grad = grad(cost, hidden_w_shared)
+	hidden_bias_grad = grad(cost, hidden_b_shared)
+
+
 	alpha = T.dscalar('alpha')
+
 	updates = [(w_shared, w_shared - alpha * weight_grad),
-	           (b_shared, b_shared - alpha * bias_grad)]
+				(hidden_w_shared, hidden_w_shared - alpha * hidden_weight_grad),
+	           (b_shared, b_shared - alpha * bias_grad),
+	           (hidden_b_shared, hidden_b_shared - alpha * hidden_bias_grad)]
 
 	train = function([inp, labels, alpha], cost, updates=updates)
 	labeled = encode_labels(train_set[1], 9)
@@ -104,7 +128,9 @@ if __name__ == '__main__':
 	            break
 	        else:
 	            temp_alpha = temp_alpha / 1.5
-
+ 	
+ 	print "len(costs)" 
+ 	print len(costs) 
 	compute_prediction = predict_func(test_set[0])
 	accuracy(compute_prediction, test_set[1])
 	
